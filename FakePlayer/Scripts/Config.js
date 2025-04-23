90c10545c07caea133a34b3bd73cc15027005278
Dr-Asm <Dr_Asm@outlook.com>
2025-04-24 02:58:49 +0800

    1.修改了配置文件名错误导致读取不到配置文件的问题。\n2.更正了调用的sender成员函数名\n3.添加了gitignore文件，删除了MAC垃圾桶

--- FakePlayer/Scripts/Config.js
+++ FakePlayer/Scripts/Config.js
@@ -5,7 +5,7 @@ var config = null;
 exports.read_config = () => {
     try {
         if (config) return config;
-        return (config = JSON.parse(file.readFileSync('config.json', 'Utf-8')));
+        return (config = JSON.parse(file.readFileSync('Config.json', 'Utf-8')));
     } catch (error) {
         if (error.code === 'ENOENT')
             console.warn('[WARNING] 没有找到配置文件，请重新下载后编辑再次尝试！');
